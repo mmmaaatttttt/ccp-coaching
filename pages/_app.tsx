bbcdcd892d36f9d8a803454431db0151d824ced6
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2022-02-12 18:34:18 -0800

    add header stub

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "../styles/variables.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Header from "common/components/header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
