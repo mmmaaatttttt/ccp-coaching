46ca1c69e2b0459f9cc2e08c67ad33d3b573abaf
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2022-02-12 19:03:22 -0800

    update header, include stubs for about/contact

---
+++ common/components/header/constants.tsx
@@ -0,0 +1,10 @@
+export const links = [
+  {
+    path: "/about",
+    text: "About"
+  },
+  {
+    path: "/contact",
+    text: "Contact"
+  }
+];
--- common/components/header/index.tsx
+++ common/components/header/index.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import Link from "next/Link";
+import logo from "public/logo.svg";
+import { links } from "./constants"
+import styles from "./Header.module.css";
+
+const title = "Conscious Connected Parenting";
+
 const Header = () => {
-  return <h1>Header</h1>
-}
+  return (
+    <header className={styles.header}>
+      <Image src={logo} alt={`${title} logo`} width={50} height={50} />
+      <h2 className={styles.title}>{title}</h2>
+      {links.map(({ path, text }) => (
+        <Link href={path} key={path}>
+          <a className={styles.link}>{text}</a>
+        </Link>
+      ))}
+    </header>
+  );
+};
 
 export default Header;
