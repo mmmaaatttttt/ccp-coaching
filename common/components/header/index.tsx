58f40a41e68ec2dba7d3f12a78453012356b42dc
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2022-02-19 19:18:13 -0800

    change hamburger nav animation

--- common/components/header/index.tsx
+++ common/components/header/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/Link";
-import { List, XLg } from "react-bootstrap-icons";
 import logo from "public/logo.svg";
 import { links } from "./constants";
 import styles from "./Header.module.css";
@@ -10,24 +9,18 @@ const title = "Conscious Connected Parenting";
 
 const Header = () => {
   const [expanded, setExpanded] = useState(false);
-  const expand = () => setExpanded(true);
-  const collapse = () => setExpanded(false);
+  const toggleExpanded = () => setExpanded(expanded => !expanded);
   return (
     <header className={`${styles.header} ${expanded ? styles.expanded : ""}`}>
       <div className={styles.logo}>
         <Image src={logo} alt={`${title} logo`} width={50} height={50} />
         <h2 className={styles.title}>{title}</h2>
       </div>
-      <List
-        size={24}
-        className={`${styles.icon} ${expanded ? styles.hideIcon : ""}`}
-        onClick={expand}
-      />
-      <XLg
-        size={24}
-        className={`${styles.icon} ${!expanded ? styles.hideIcon : ""}`}
-        onClick={collapse}
-      />
+      <div className={styles.menu}>
+        <a className={expanded ? styles.expanded : ""} onClick={toggleExpanded}>
+          <i>Menu</i>
+        </a>
+      </div>
       <div
         className={`${styles.links} ${expanded ? styles.expandedLinks : ""}`}
       >
