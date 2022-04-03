dd9a6bf37f06b70c6e6730b57848354f55397e66
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2022-04-02 17:51:48 -0700

    more edits to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,15 +16,37 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section className={styles.introduction}>
-        <div>
+      <section className={styles.section}>
+        <div className={`${styles.col2} ${styles.imageWrapper}`}>
           <Image src={topImage} alt="Woman and child overlooking a lake" />
         </div>
-        <div className={styles.secondary}>
+        <div className={`${styles.secondary} ${styles.col2}`}>
           <h1>
             Find joy in parenting, <br />
             through connection and unconditional love
           </h1>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <div className={`${styles.col2} p-5`}>
+          <h2 className="center">
+            Do you find yourself being regularly triggered by your children?
+          </h2>
+          <p className="mx-5">
+            Do you react out of anger and later feel full of guilt and remorse?
+            Are you longing to change this pattern but are not sure how?
+          </p>
+          <p className="mx-5">
+            I am here to support you to connect with the why behind your
+            triggers so that you are able to make sense of the patterns at play
+            and create new conscious ways of responding and experiencing
+            parenting.
+          </p>
+          <p className="mx-5">
+            Conscious connected parenting is about intention and commitment to
+            parenting your children with unconditional love that is rooted in
+            science and understanding. It’s about progress, not perfection!
+          </p>
         </div>
       </section>
     </>
