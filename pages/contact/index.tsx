d1eec9868ccc4fc3dfe209bfb0b41d6f1a109976
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2022-04-03 21:24:16 -0700

    add calendly integration to contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import { InlineWidget } from "react-calendly";
 
 const Contact: NextPage = () => {
-  return <div>Contact</div>
+  return (
+    <div>
+      <InlineWidget url="https://calendly.com/ccpcoachmeg/30min" />
+    </div>
+  );
 };
 
 export default Contact;
