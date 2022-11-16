cae989481f583925229642e0e22ab97a8ef81bc5
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2022-11-15 21:51:52 -0800

    fix head warning

--- common/components/base_head/index.tsx
+++ common/components/base_head/index.tsx
@@ -4,15 +4,18 @@ type BaseHeadProps = {
   prefix: String;
 };
 
-const BaseHead = ({ prefix }: BaseHeadProps) => (
-  <Head>
-    <title>{prefix} - Conscious Connected Parenting</title>
-    <meta
-      name="description"
-      content="Conscious Connected Parenting - parent coaching services provided by Megan Lane."
-    />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-);
+const BaseHead = ({ prefix }: BaseHeadProps) => {
+  const title = `${prefix} - Conscious Connected Parenting`;
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta
+        name="description"
+        content="Conscious Connected Parenting - parent coaching services provided by Megan Lane."
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
 
 export default BaseHead;
