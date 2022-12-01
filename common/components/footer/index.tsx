4ef9933d44acfd12d742c1d6a6ce0f1dbff64057
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2022-11-30 21:50:16 -0800

    add socials to footer

--- common/components/footer/index.tsx
+++ common/components/footer/index.tsx
@@ -1,9 +1,13 @@
+import Socials from "../socials";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      © {new Date().getFullYear()} Conscious Connected Parenting
+      <div className="p-2">© {new Date().getFullYear()} Conscious Connected Parenting</div>
+      <div className="p-2">
+        <Socials />
+      </div>
     </footer>
   );
 };
